40a1eaf7d337754ebfb9cd21f0b5bdf317ad263a
Angelstorm1986 <matteo.nichelini@gmail.com>
2022-05-10 20:14:04 +0200

    Index: creato scaffholding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import AppSearch from './components/AppSearch.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppSearch
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./style/vars.scss";
+@import "./style/general.scss";
+
+</style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'AppSearch',
+  
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
