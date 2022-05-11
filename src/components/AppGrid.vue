283a07f6f83978b3b97a698614134d8e5ed5e375
Angelstorm1986 <matteo.nichelini@gmail.com>
2022-05-11 15:40:22 +0200

    spaces

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -3,13 +3,13 @@
         <h2>{{title}}</h2>
         <ul>
             <li v-for="(item) in items" :key="item.id">
-            id: {{item.id}}
-            titolo originale: {{item.original_title ? item.original_title : item.original_name}}
-            titolo: {{item.title ? item.title : item.name}}
+            id: {{item.id}}<br>
+            titolo originale: {{item.original_title ? item.original_title : item.original_name}}<br>
+            titolo: {{item.title ? item.title : item.name}}<br>
             lingua: {{item.original_language}}
             <img v-if="item.original_language === 'en' || item.original_language === 'it'" :src="require('../assets/images/' + item.original_language + '.jpg')" :alt="'Bandiera' + item.original_language">
-            <img v-else :src="require('../assets/images/' + flag + '.jpg')" :alt="'Bandiera' + flag">
-            voto: {{item.vote_average}}
+            <img v-else :src="require('../assets/images/' + flag + '.jpg')" :alt="'Bandiera' + flag"><br>
+            voto: {{item.vote_average}}<br>
             </li>
         </ul>
     </section>
@@ -33,6 +33,6 @@ export default {
 
 <style lang="scss">
 img{
-    max-width: 100px;
+    max-width: 20px;
 }
 </style>
